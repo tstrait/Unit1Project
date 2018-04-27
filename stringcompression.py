740662c7a22336e4d1c5ccd2e5ddebf3598a2f62
Talia Strait <testrait@gmail.com>
2018-04-27 14:13:02 -0500

    Update responses

--- stringcompression.py
+++ stringcompression.py
@@ -1,25 +1,30 @@
-s='aaaabbbccddfazz'
-a= []
-newlist = []
-count=0
+mystring = 'abbbcccdddde'
 
-for i in range(0,len(s)):
-    a.append(s[i])
-    if len(set(a)) == 1:
-        count +=1
-        if i == len(s)-1:
-            itemcount = a[0] + str(count)
-            newlist.append(itemcount)
-    elif len(set(a)) > 1:
-        itemcount = a[0] + str(count)
-        newlist.append(itemcount)
-        del a[0:len(a)-1]
-        count=1
-
-print(newlist)
-newlist2 = ''.join(newlist)
-
-if len(s) < len(newlist2):
-    print('Original string was shorter:' + s)
-else:
-    print('Compressed list is shorter: ' + newlist2)
+def string_compression(mystring):
+    print("Original string:", mystring)
+    a=[]
+    compressed=[]
+    #for each character in string, check if next character is the same or not
+    for i in range(0,len(mystring)-1):
+        #if next character is the same, add to a temporary string
+        if mystring[i] == mystring[i+1]:
+            a.append(mystring[i+1])
+        #if not, add current character and count the length of group of same characters; add that to the compressedstring list
+        else:
+            a.append(mystring[i])
+            compresseditem = a[0] + str(len(a))
+            compressed.append(compresseditem)
+            a=[]
+    #add the last character
+    a.append(mystring[-1:])
+    compresseditem = a[0] + str(len(a))
+    compressed.append(compresseditem)
+    #join list elements into a compressed string
+    compressedstring = ''.join(compressed)
+    #only return compressed version if it's shorter than original string
+    if len(compressedstring) >= len(mystring):
+        print("Original string is shorter than compressed:", mystring)
+    else:
+        print("Compressed string:", compressedstring)
+    
+string_compression(mystring)
